7431274f9d4adc0c1864280927675b0390fc46cd
Noa Loureiro <74070575+minyavasaiel@users.noreply.github.com>
2023-09-15 13:15:30 +0200

    Crud de organizaciones listo

--- prueba_tecnica/views.py
+++ prueba_tecnica/views.py
@@ -52,7 +52,7 @@ def read_json():
 def listar(request):
     #obtener todos los elementos
     organizations = Organization.objects.all()
-    return render(request, 'listar.html', {'organizations': organizations})
+    return render(request, 'organizations-list.html', {'organizations': organizations})
 
 def listarCentros(request):
     centers = Center.objects.all()
@@ -61,6 +61,9 @@ def listarCentros(request):
 def add_center(request):
     organizations = Organization.objects.all()
     return render(request, 'add-center.html', {'organizations': organizations})
+
+def add_organization(request):
+    return render(request, 'add-organization.html', {})
 
 def added_center(request):
     if request.method != 'POST':
@@ -71,12 +74,26 @@ def added_center(request):
     organization = Organization.objects.get(pk=idOrganization)
     newCenter = Center(name=name, stories=stories, organization=organization)
     newCenter.save()
-    return HttpResponse("Centro agregado correctamente")
+    return HttpResponse("Centro agregado correctamente.")
+
+def added_organization(request):
+    if request.method != 'POST':
+        return HttpResponse('Error')
+    name = request.POST['name']
+    contact = request.POST['contact']
+    phone= request.POST['phone']
+    newOrganization = Organization(name=name, contact=contact, phone=phone)
+    newOrganization.save()
+    return HttpResponse("Organización agregada correctamente.")
 
 def detail_center(request, id):
     center = Center.objects.get(pk=id)
     organizations = Organization.objects.all()
     return render(request, 'detail-center.html', {'center': center, 'organizations': organizations})
+
+def detail_organization(request, id):
+    organization = Organization.objects.get(pk=id)
+    return render(request, 'detail-organization.html', {'organization': organization})
 
 def updated(request, id):
     if request.method != 'POST':
@@ -90,13 +107,29 @@ def updated(request, id):
     newOrganization = Organization.objects.get(pk=idOrganization)
     center.organization = newOrganization
     center.save()
-    return render(request, 'updated.html', {'center': center, 'newOrganization': newOrganization})
+    return render(request, 'updated-center.html', {'center': center, 'newOrganization': newOrganization})
+
+def updated_organization(request, id):
+    if request.method != 'POST':
+        return HttpResponse('Error')
+    organization = Organization.objects.get(pk=id)
+    organization.name = request.POST['name']
+    organization.contact = request.POST['contact']
+    organization.phone = request.POST['phone']
+    organization.save()
+    return render(request, 'updated-organization.html', {'organization': organization})
 
 @login_required     #para evitar que borren sin estar logueados, poniendo la url
 def delete_center(request, id):
     center = Center.objects.get(pk=id)
     center.delete()
     return render(request, 'deleted-center.html', {'center': center})
+
+@login_required     #para evitar que borren sin estar logueados, poniendo la url
+def delete_organization(request, id):
+    organization = Organization.objects.get(pk=id)
+    organization.delete()
+    return render(request, 'deleted-organization.html', {'organization': organization})
         
     
   
